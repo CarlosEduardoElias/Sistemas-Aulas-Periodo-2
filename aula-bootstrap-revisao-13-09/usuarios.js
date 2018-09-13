29948e204c006a33e186a99ba8e4cecabcd1b620
EduardoS <carloseduardoeats@gmail.com>
2018-09-13 00:22:18 -0300

    Edit

---
+++ aula-bootstrap-revisao-13-09/usuarios.js
@@ -0,0 +1,31 @@
+const usuarios=[
+	
+		{
+		id:'1',
+		nome:'josefa',
+		chave:'5488484',
+		email:'arroba@anderline'
+		},
+
+		{
+		id:'2',
+		nome:'Josemedia',
+		chave:'545454',
+		email:'arroba@anderline'
+		},
+
+		{
+		id:'3',
+		nome:'Josegrossa',
+		chave:'259251',
+		email:'arroba@anderline'
+		},
+
+		{
+		id:'4',
+		nome:'Josefina',
+		chave:'259251',
+		email:'arroba@anderline'
+		}
+]
+
